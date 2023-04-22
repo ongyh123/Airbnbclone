d3192bde327bd60bfbb945203c07192202e0bd14
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-04-22 17:52:28 +0800

    set up tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
