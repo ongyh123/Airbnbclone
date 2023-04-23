f2eaf93430dfc8798c55273c02ce6028c35edb44
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-04-23 11:58:53 +0800

    wrap Nav with ClientOnly component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,31 @@
-import { Nunito } from "next/font/google";
+import { Nunito } from 'next/font/google';
 
-import './globals.css'
-import Navbar from "./components/navbar/Navbar";
+import './globals.css';
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
 
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
-}
+};
 
-const font = Nunito ({
-  subsets:['latin']
+const font = Nunito({
+  subsets: ['latin'],
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={font.className}>
-        <Navbar />
-        {children}</body>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
