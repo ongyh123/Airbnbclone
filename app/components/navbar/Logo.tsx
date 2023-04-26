d8193fbf2c3c8da64a79ea6ad5ec30db302207ac
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-04-26 21:37:05 +0800

    create category UI

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,19 +1,21 @@
-"use client";
+'use client';
 
-import Image from "next/image"
-import { useRouter } from "next/navigation"
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-const Logo = () =>{
-    const router = useRouter();
+const Logo = () => {
+  const router = useRouter();
 
-    return (
-        <Image 
-        alt="logo" 
-        className="hidden md:block cursor-pointer"
-        height="100"
-        width="100" 
-        src="/images/logo.png"/>
-    );
-}
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      alt='logo'
+      className='hidden md:block cursor-pointer'
+      height='100'
+      width='100'
+      src='/images/logo.png'
+    />
+  );
+};
 
 export default Logo;
